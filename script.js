64b398e321ee475512836ff9aa9d9b13974e287c
adityabisoyi <109476995+adityabisoyi@users.noreply.github.com>
2022-08-31 01:11:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,97 +24,16 @@ function del() {
     }
 }
 
-
-function press1() {
+function numPress(num) {
     if(opr !== 0 && disp_el.textContent == num1 ) {
         disp_el.textContent = ""
     }
     else if(disp_el.textContent === "0") {
         disp_el.textContent = ""
     }
-    disp_el.textContent += 1
+    disp_el.textContent += num
 }
-function press2() {
-    if(opr !== 0 && disp_el.textContent == num1) {
-        disp_el.textContent = ""
-    }
-    else if(disp_el.textContent === "0") {
-        disp_el.textContent = ""
-    }
-    disp_el.textContent += 2
-}
-function press3() {
-    if(opr !== 0 && disp_el.textContent == num1) {
-        disp_el.textContent = ""
-    }
-    else if(disp_el.textContent === "0") {
-        disp_el.textContent = ""
-    }
-    disp_el.textContent += 3
-}
-function press4() {
-    if(opr !== 0 && disp_el.textContent == num1) {
-        disp_el.textContent = ""
-    }
-    else if(disp_el.textContent === "0") {
-        disp_el.textContent = ""
-    }
-    disp_el.textContent += 4
-}
-function press5() {
-    if(opr !== 0 && disp_el.textContent == num1) {
-        disp_el.textContent = ""
-    }
-    else if(disp_el.textContent === "0") {
-        disp_el.textContent = ""
-    }
-    disp_el.textContent += 5
-}
-function press6() {
-    if(opr !== 0 && disp_el.textContent == num1) {
-        disp_el.textContent = ""
-    }
-    else if(disp_el.textContent === "0") {
-        disp_el.textContent = ""
-    }
-    disp_el.textContent += 6
-}
-function press7() {
-    if(opr !== 0 && disp_el.textContent == num1) {
-        disp_el.textContent = ""
-    }
-    else if(disp_el.textContent === "0") {
-        disp_el.textContent = ""
-    }
-    disp_el.textContent += 7
-}
-function press8() {
-    if(opr !== 0 && disp_el.textContent == num1) {
-        disp_el.textContent = ""
-    }
-    else if(disp_el.textContent === "0") {
-        disp_el.textContent = ""
-    }
-    disp_el.textContent += 8
-}
-function press9() {
-    if(opr !== 0 && disp_el.textContent == num1) {
-        disp_el.textContent = ""
-    }
-    else if(disp_el.textContent === "0") {
-        disp_el.textContent = ""
-    }
-    disp_el.textContent += 9
-}
-function press0() {
-    if(opr !== 0 && disp_el.textContent == num1) {
-        disp_el.textContent = ""
-    }
-    else if(disp_el.textContent === "0") {
-        disp_el.textContent = ""
-    }
-    disp_el.textContent += 0
-}
+
 function presspoint() {
     disp_el.textContent += "."
 }
